724c8def862f36279b79c63c4255c2e5353c84f1
Abel Bima Wiratama <learn.smktelkom-mlg.sch.id>
2016-09-11 08:20:29 +0700

    Menuliskan Script pada MainActivity

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3001/tugas001/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3001/tugas001/MainActivity.java
@@ -2,12 +2,27 @@ package id.sch.smktelkom_mlg.tugas01.xirpl3001.tugas001;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama, etAlamat, etType;
+    TextView tvHasil, tvBonus;
+    Button bOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etAlamat = (EditText) findViewById(R.id.editTextAlamat);
+        etType = (EditText) findViewById(R.id.editTextType);
+
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvBonus = (TextView) findViewById(R.id.textViewBonus);
+
+        bOK = (Button) findViewById(R.id.buttonOK);
     }
 }
