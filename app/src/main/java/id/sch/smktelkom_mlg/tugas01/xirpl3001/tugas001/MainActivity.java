73dd01b245bc3f21b1d3809713e69182bbc245c9
Abel Bima Wiratama <learn.smktelkom-mlg.sch.id>
2016-09-11 17:11:30 +0700

    Mengganti Spinner

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3001/tugas001/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3001/tugas001/MainActivity.java
@@ -3,27 +3,20 @@ package id.sch.smktelkom_mlg.tugas01.xirpl3001.tugas001;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class MainActivity extends AppCompatActivity {
     EditText etNama, etAlamat, etType;
-    TextView tvHasil, tvBonus;
+    TextView tvHasil, tvBonus, tvProvinsi, tvKota;
     Spinner spProvinsi, spKota;
-    String[][] arKota = {{"Kediri", "Malang", "Blitar", "Tulungagung"}, {"Surakarta", "Klaten", "Yogyakarta"}, {"Bandung", "Depok", "Cirebon"}};
-
-    ArrayList<String> listKota = new ArrayList<>();
-    ArrayAdapter<String> adapter;
-    RadioButton rbS, rbU, rbM;
+    CheckBox cbH, cbJ, cbJH;
+    RadioButton rb;
     RadioGroup rgType;
     Button bOK;
 
@@ -31,58 +24,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         etNama = (EditText) findViewById(R.id.editTextNama);
         etAlamat = (EditText) findViewById(R.id.editTextAlamat);
         etType = (EditText) findViewById(R.id.editTextType);
-
         spProvinsi = (Spinner) findViewById(R.id.spinnerProvinsi);
         spKota = (Spinner) findViewById(R.id.spinnerKota);
-
-        rbS = (RadioButton) findViewById(R.id.radioButtonS);
-        rbU = (RadioButton) findViewById(R.id.radioButtonU);
-        rbM = (RadioButton) findViewById(R.id.radioButtonM);
         rgType = (RadioGroup) findViewById(R.id.radioGroupType);
-
+        cbH = (CheckBox) findViewById(R.id.checkBoxH);
+        cbJ = (CheckBox) findViewById(R.id.checkBoxJ);
+        cbJH = (CheckBox) findViewById(R.id.checkBoxJH);
+        bOK = (Button) findViewById(R.id.buttonOK);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
         tvBonus = (TextView) findViewById(R.id.textViewBonus);
-
-        bOK = (Button) findViewById(R.id.buttonOK);
+        tvProvinsi = (TextView) findViewById(R.id.textViewProvinsi);
+        tvKota = (TextView) findViewById(R.id.textViewKota);
 
         bOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 doProses();
+                doPilih();
+                doClick();
+
             }
         });
-
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listKota);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spKota.setAdapter(adapter);
-
-        spProvinsi.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
-                listKota.clear();
-                listKota.addAll(Arrays.asList(arKota[pos]));
-                adapter.notifyDataSetChanged();
-                spKota.setSelection(0);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-
 
     }
 
+    private void doClick() {
+        String hasil = null;
+
+        if (rgType.getCheckedRadioButtonId() != -1) {
+            RadioButton rb = (RadioButton) findViewById(rgType.getCheckedRadioButtonId());
+            hasil = rb.getText().toString();
+        }
+
+        if (hasil == null) {
+            tvHasil.setText("Belum Mengisi Type Motor");
+        } else {
+            tvHasil.setText("Type Motor Anda : " + hasil);
+        }
+    }
 
     private void doProses() {
         if (isValid()) {
             String nama = etNama.getText().toString();
             String alamat = etAlamat.getText().toString();
-            tvHasil.setText("Nama " + nama + " Beralamat " + alamat + " Kota " + spKota.getSelectedItem().toString() + " Provinsi " + spProvinsi.getSelectedItem().toString());
+            tvHasil.setText(" Nama : " + nama + "\n Alamat  : " + alamat +
+                    "\n Pengiriman ke Provinsi : " + spProvinsi.getSelectedItem().toString() +
+                    "\n Ke Kota " + spKota.getSelectedItem().toString());
+
         }
     }
 
@@ -91,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
 
         String nama = etNama.getText().toString();
         String alamat = etAlamat.getText().toString();
+
 
         if (nama.isEmpty()) {
             etNama.setError("Nama Belum Diisi");
@@ -113,4 +105,10 @@ public class MainActivity extends AppCompatActivity {
 
         return valid;
     }
+
+    private void doPilih() {
+
+    }
+
+
 }
