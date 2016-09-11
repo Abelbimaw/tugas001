909504365e4debef73e23e38b3a3056fa8ab6473
Abel Bima Wiratama <learn.smktelkom-mlg.sch.id>
2016-09-11 10:14:12 +0700

    Menuliskan Script untuk Spinner

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3001/tugas001/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3001/tugas001/MainActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.tugas01.xirpl3001.tugas001;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,5 +25,48 @@ public class MainActivity extends AppCompatActivity {
         tvBonus = (TextView) findViewById(R.id.textViewBonus);
 
         bOK = (Button) findViewById(R.id.buttonOK);
+
+        bOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doProses();
+            }
+        });
+    }
+
+    private void doProses() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            String alamat = etAlamat.getText().toString();
+            tvHasil.setText(nama + " Beralamat " + alamat);
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+        String alamat = etAlamat.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama Belum Diisi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama Minimal 3 Karakter");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+        if (alamat.isEmpty()) {
+            etAlamat.setError("Alamat Belum Diisi");
+            valid = false;
+        } else if (alamat.length() < 3) {
+            etAlamat.setError("Nama Minimal 3 Karakter");
+            valid = false;
+        } else {
+            etAlamat.setError(null);
+        }
+
+        return valid;
     }
 }
